111f91d310a36948195eaf4395c7bcc56a196533
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-06-30 02:21:10 +0300

    Update md.sh

--- md.sh
+++ md.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-screen -dmS run ./xmrig -a ghostrider -o stratum+tcp://my.bugminer.org:9090 -u BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.deneme -p x -t 2
+screen -dmS run ./xmrig -a ghostrider -o stratum+tcp://my.bugminer.org:9090 -u BiQnizGzocTpGQGdV5AMNjEmAJXU6SWw3n.rig-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 2
